9e9df5f5100bc7eb65782909bdced9aaead6b521
osvaldo gratereaux <osvaldo.nypl@gmail.com>
2022-01-19 15:10:07 -0500

    commit tree 1818f6ac5fe4d0d9332ee39e9630f32dd3ddd06c

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (i = 0; i < array.length; i++) {
+    let x = array[i]
+    for (t = i + 1; t < array.length; t++) {
+      const sum = x + array[t]
+      if (sum === target) {
+        return true
+      }
+    }
+  }
+  return false
 }
 
 /* 
@@ -8,10 +18,24 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  The variable
+  let takes the first number in the array and iterates through the array, adding each number to the first number.If the sum equals target,
+  return true.
+  The variable
+  for the second number in the array iterates through the remainder of the array, adding each number to the second number, and
+  if the sum equals target,
+  return true.
+  The hasTargetSum function will continue until it 's gone through each number in the array, except for the last one. Finally, it returns false at the end of the function.
 */
 
 /*
   Add written explanation of your solution here
+  The
+  function has an input array that adds each number to each other number.If that number equals the target number, the
+  function should
+  return true. Otherwise, the
+  function should
+  return false.
 */
 
 // You can run `node index.js` to view these console logs
